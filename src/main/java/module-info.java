04b900b162b1a3dd3c27636f2cc02bcec8777f20
JAO300 <jonadal02@gmail.com>
2025-03-31 21:25:01 +0000

    test bras

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module hopur_h {
     requires javafx.controls;
     requires javafx.fxml;
 
+
     opens hopur_h to javafx.fxml;
     exports hopur_h;
 }
---
+++ src/main/test/java/hopur_h/SearchControllerTest.java
@@ -0,0 +1,15 @@
+package hopur_h;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.*;
+
+import java.util.*;
+
+public class SearchControllerTest {
+
+}
+
