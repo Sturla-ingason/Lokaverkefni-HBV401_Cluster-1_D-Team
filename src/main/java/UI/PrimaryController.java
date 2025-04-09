9901ce086d265dcd25dbc8a2276db5be6e22a164
olafurkristofer <olafur.k123@gmail.com>
2025-04-09 10:24:57 +0000

    Uppfæra tour display

--- src/main/java/UI/PrimaryController.java
+++ src/main/java/UI/PrimaryController.java
@@ -11,8 +11,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import javafx.scene.control.ListView;
 import Data.BookingDB;
 import Data.TourDB;
@@ -38,7 +44,9 @@ public class PrimaryController {
     @FXML
     private ListView<String> tourListView;
 
-
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
     
     private List<Tour> tours = new ArrayList<>();
     BookingDB test = new BookingDB();
@@ -64,5 +72,33 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         test.getAllBookings();
+    }
+
+    public void displayTour(MouseEvent event) throws IOException{
+        String selectedName = tourListView.getSelectionModel().getSelectedItem();
+        System.out.println(selectedName);
+
+        Tour selectedTour = null;
+
+        for (Tour tour : tours){
+            if (tour.getTourName().equals(selectedName)) {
+                selectedTour = tour;
+                break;
+            }
+        }
+
+        System.out.println(selectedTour);
+        if (selectedTour != null) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/tour.fxml"));
+            root = loader.load();
+            TourDisplayController tourDisplay = loader.getController();
+            tourDisplay.inputDetails(selectedTour);
+            
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+
     }
 }
